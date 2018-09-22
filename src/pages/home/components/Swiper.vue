bbee1d79d1f2525893de01aab363de9d4dc5c5b3
fengshiying <577829449@qq.com>
2018-09-22 11:58:05 +0800

    eight

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,11 +1,9 @@
 <template>
 <div class="wrapper">
-  <swiper :options="swiperOption">
-    <!-- slides -->
-    <swiper-slide v-for="img in swiperList" :key="img.id">
-      <img class="swiper-img" :src="img.imgUrl">
+  <swiper :options="swiperOption" v-if="showSwiper">
+    <swiper-slide v-for="item in list" :key="item.id">
+      <img class="swiper-img" :src="item.imgUrl">
     </swiper-slide>
-
     <div class="swiper-pagination"  slot="pagination"></div>
   </swiper>
 </div>
@@ -14,31 +12,39 @@
 <script>
     export default {
         name: "HomeSwiper",
+      props: {
+        list: Array
+      },
       data (){
           return{
             swiperOption:{
-              pagination:".swiper-pagination",
-              loop:true
+              pagination: '.swiper-pagination',
+              loop: true
             },
             swiperList:[
-              {id:"001",imgUrl:"https://img1.qunarzz.com/qs/1808/c1/96aead8f8ed82902.jpg"},
-              {id:"002",imgUrl:"https://img1.qunarzz.com/qs/1809/2b/f060d9035028da02.jpg"},
-              {id:"003",imgUrl:"https://img1.qunarzz.com/qs/1808/f1/5237b90429129102.jpg"},
+              {id:"001",imgUrl:"http://img1.qunarzz.com/piao/fusion/1809/86/deb33800de224c02.jpg_750x200_74fb795e.jpg"},
+              {id:"002",imgUrl:"http://img1.qunarzz.com/piao/fusion/1805/e8/14b75b1c81fbe702.jpg_750x200_e6d4f1f1.jpg"},
+              {id:"003",imgUrl:"http://img1.qunarzz.com/piao/fusion/1809/86/deb33800de224c02.jpg_750x200_74fb795e.jpg"},
               ]
           }
+      },
+      computed:{
+        showSwiper(){
+          return this.list.length;
+        }
       }
     }
 </script>
 
 <style scoped lang="stylus">
   .wrapper >>> .swiper-pagination-bullet-active{
-    background :white !important
+    background :white
   }
   .wrapper{
     width :100%;
     height :0;
     overflow :hidden;
-    padding-bottom: 54%;
+    padding-bottom: 26%;
     background :#eee
   }
 
